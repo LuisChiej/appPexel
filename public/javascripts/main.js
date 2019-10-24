0def624b4d0aefe11ea8f6f792faafcbcca277fc
unknown <luis.chiej.lc@gmail.com>
2019-10-25 00:17:06 +0100

    update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,31 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/swk.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/swk.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
+    });
+
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', e => {
+      deferredPrompt = e;
+
+      setTimeout(() => {
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+          .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+          });
+      }, 5000)
     });
   });
 }
--- public/swk.js
+++ public/swk.js
@@ -1,15 +1,51 @@
 // The install handler takes care of precaching the resources we always need.
+const PRECACHE = 'precache-v2';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = ['/stylesheets/app.css', '/stylesheets/style.css',
+    '/javascripts/app.js', '/javascripts/choices.js', '/javascripts/main.js', '/favicon.ico'];
+
 self.addEventListener('install', event => {
-   console.log('installed');
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-    console.log('activated');
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
 });
 
 // The fetch handler serves responses for same-origin resources from a cache.
 self.addEventListener('fetch', event => {
     // Skip cross-origin requests, like those for Google Analytics.
-    console.log('fetched');
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
 });
